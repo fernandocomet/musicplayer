bf89378cf69e5de3ec11e712b06015cd6a60a083
fernandocomet <fernandocomet@gmail.com>
2020-11-09 17:24:40 +0100

    Applying CSS to player

--- musicplayer/src/App.js
+++ musicplayer/src/App.js
@@ -12,25 +12,36 @@ import Like from './components/Like';
 import Volume from './components/Volume';
 import Menu from './components/Menu';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Timeline></Timeline>
-        <Shuffle></Shuffle>
-        <Loop></Loop>
-        <Previous></Previous>
-        <Play></Play>
-        <Next></Next>
-        <Time></Time>
-        <Caratule></Caratule>
-        <Like></Like>
-        <Volume></Volume>
-        <Menu></Menu>
-      </header>
+     
+        <div className="progressbar"><Timeline></Timeline></div>
+        <div className="playercontainer">
+          <div className="left">
+            <div className="player">
+              <div className="player-shuffle"><Shuffle></Shuffle></div>
+              <div className="player-loop"><Loop></Loop></div>
+              <div className="player-previous"><Previous></Previous></div>
+              <div className="player-play"><Play></Play></div>
+              <div className="player-next"><Next></Next></div>
+            </div>
+            <div className="timecontainer"><Time></Time></div> 
+          </div>
+          <div className="center">
+              <div className="center-caratule"><Caratule></Caratule></div>
+              <div className="center-like"><Like></Like></div>
+          </div>
+          <div className="right">
+            <div className="volumecontainer"><Volume></Volume></div>
+            <div className="menucontainer"><Menu></Menu></div>
+          </div>
+        </div>
+
+   
     </div>
-  );
+  )
 }
 
 export default App;
